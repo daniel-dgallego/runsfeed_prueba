c5887961d31ed4899a28ccd2aa4f12949543111d
Daniel <1asir.daniel.gallego@gmail.com>
2016-05-25 20:46:38 +0200

    -icon app -Celdas user, datos de carrera -add cocoaPods con google maps SDK

---
+++ Runsfeed/CellMap.swift
@@ -0,0 +1,24 @@
+//
+//  CellMap.swift
+//  Runsfeed
+//
+//  Created by Daniel Gallego Peralta on 25/5/16.
+//  Copyright © 2016 runator.com. All rights reserved.
+//
+
+import UIKit
+
+class CellMap: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
